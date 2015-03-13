d8520235fe00aae9daef3615ecd4c69d80e034bf
drexedam <drexedam@gmail.com>
2015-03-13 20:55:20 +0100

    - Remove unused Changes - Javadoc

--- at.fhv.tedapt/src/at/fhv/tedapt/flyway/change/CopyColumn.java
+++ at.fhv.tedapt/src/at/fhv/tedapt/flyway/change/CopyColumn.java
@@ -1,15 +1,32 @@
 package at.fhv.tedapt.flyway.change;
 
+import at.fhv.tedapt.flyway.entity.Column;
+
+/**
+ * 
+ * @author Damian Drexel
+ * @version 0.1
+ */
 public class CopyColumn implements Change {
 
 	private String _table;
 	private String _origin;
 	private String _goal;
 	
+	/**
+	 * 
+	 * @param table The table to operate on
+	 * @param origin The column containing the data to be copied
+	 * @param goal The column to copy the data to
+	 */
 	public CopyColumn(String table, String origin, String goal) {
 		_table = table.toLowerCase();
 		_goal = goal.toLowerCase();
 		_origin= origin.toLowerCase();
+	}
+	
+	public CopyColumn(String table, Column origin, Column goal) {
+		this(table, origin.getName(), goal.getName());
 	}
 
 	@Override
--- at.fhv.tedapt/src/at/fhv/tedapt/flyway/change/CopyTable.java
+++ at.fhv.tedapt/src/at/fhv/tedapt/flyway/change/CopyTable.java
@@ -1,10 +1,21 @@
 package at.fhv.tedapt.flyway.change;
 
+/**
+ * Copy one reference table into another
+ * @author Damian Drexel
+ * @version 0.1
+ */
 public class CopyTable implements Change {
 
 	private String _goal;
 	private String _origin;
 	
+	
+	/**
+	 * 
+	 * @param origin Source for data table
+	 * @param goal The table the data has to be copied to
+	 */
 	public CopyTable(String origin, String goal) {
 		_goal = goal;
 		_origin = origin;
